d36ecf5a49302abd7a944d84b4a1f41951abd31c
Abhishek G Bhat <abhishek.bhat@go-jek.com>
2021-02-17 08:11:44 +0530

    Some changes

--- examples/set-title.go
+++ examples/set-title.go
@@ -1,44 +1,78 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	force "github.com/ForceCLI/force/lib"
 	batch "github.com/octoberswimmer/batchforce"
+	"log"
+	"os"
+	"strings"
 	"time"
 )
 
-var (
-	fiftyYearsAgo  = time.Now().AddDate(-50, 0, 0)
-	thirtyYearsAgo = time.Now().AddDate(-30, 0, 0)
-)
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file " + filePath, err)
+	}
+	defer f.Close()
 
-func main() {
-	query := `
-		SELECT
-			Id,
-			Birthdate
-		FROM
-			Contact
-		WHERE
-			Birthdate != null
-	`
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for " + filePath, err)
+	}
 
-	batch.Run("Contact", query, setTitle)
+	return records
 }
 
-func setTitle(record force.ForceRecord) (updates []force.ForceRecord) {
-	birthdate, err := time.Parse("2006-01-02", record["Birthdate"].(string))
-	if err != nil {
-		return
+func main() {
+	records := readCsvFile("/Users/abhishekgbhat/GolandProjects/batchforce/examples/SheetData.csv")
+	var processRecords string
+	var a int = 0
+	for value := range records {
+		a = a + 1
+		processRecords = processRecords + fmt.Sprintf(
+			"'%v',",
+			records[value],
+		)
+		if (a >= 630) {
+			a = 0
+			processRecords = strings.ReplaceAll(processRecords, "[", "")
+			processRecords = strings.ReplaceAll(processRecords, "]", "")
+			processRecords = processRecords[:len(processRecords)-1]
+			println(len(processRecords))
+			println(processRecords)
+			process(processRecords)
+			time.Sleep(1 * time.Second)
+			processRecords = ""
+		}
+
+
 	}
+}
+
+
+func process(recordsProcess string) {
+
+	query := `select 
+				Id, Name, Inactive_Reason__c, Active__c, Inactive_Reason_Detail__c,
+				Outlet__r.Outlet_Midtrans_MID__c 
+			  from 
+				Account 
+			  where 
+				Product_Outlet_Record_Type__c = 'Product Outlet (GO-PAY)'
+				and Outlet__r.Outlet_Midtrans_MID__c in (` + recordsProcess  + `)`
+	batch.Run("Account", query, updateRecords)
+}
+
+func updateRecords(record force.ForceRecord) (updates []force.ForceRecord) {
 	update := force.ForceRecord{}
 	update["Id"] = record["Id"].(string)
-	switch {
-	case birthdate.Before(fiftyYearsAgo):
-		update["Title"] = "Geezer"
-		updates = append(updates, update)
-	case birthdate.After(thirtyYearsAgo):
-		update["Title"] = "Whippersnapper"
-		updates = append(updates, update)
-	}
+	update["Active__c"] = false
+	update["Inactive_Reason__c"] = "Others"
+	update["Inactive_Reason_Detail__c"] = "Inactive (0 tx in 6 months) P1"
+	updates = append(updates, update);
 	return
 }
